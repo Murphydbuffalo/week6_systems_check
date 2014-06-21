b3ae9280abaeb1581bf5e65ce0af79ee2f492d65
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-21 13:54:09 -0400

    Fixed references to incorrect params value, needed to use question_id instead of id to identify questions within nested routes.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,8 +10,8 @@ class ReviewsController < ApplicationController
   end
 
   def create
-  	@review = review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:id])
+  	@review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
   	if @review.save 
   	  redirect_to root_path
   	else
@@ -23,6 +23,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-  	params.require(:review).permit(:rating)
+  	params.require(:review).permit(:rating, :body)
   end
 end
