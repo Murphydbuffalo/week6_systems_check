58fe5d8e6ca85b652e00f45a18dec50d8db4d978
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-21 12:53:14 -0400

    Added a layout view, and index, show and new views for restaurants.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+shenanigans = Restaurant.create(
+	name: 'Shenanigans',
+	address: '187 Farva Lane',
+	city: 'Mumford',
+	state: 'NY',
+	zip_code: '12345',
+	description: 'We like cheese too!',
+	category: 'American')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
